5f159d8a2dc7c0e93a009b3fd4ddba5bd6f5f810
Flashtynooo <157314861+Flashtynooo@users.noreply.github.com>
2026-08-08 03:51:18 +0200

    Add license comments to .lua file
    Added license information and usage terms for Flshty-Hub.

--- .lua
+++ .lua
@@ -1,1 +1,13 @@
+--[[
+    Flshty-Hub © 2026 par Flashtynooo. Tous droits réservés.
+    Projet : Flshty-Hub
+    Licence : Licence propriétaire d'utilisation limitée (Proprietary License)
+    
+    L'utilisation de ce code est autorisée gratuitement sous sa forme originale.
+    Toute modification, redistribution, revente ou création de versions dérivées
+    sans l'autorisation écrite de l'auteur est STRICTEMENT INTERDITE.
+    
+    Pour lire la licence complète : https://github.com/Flashtynooo/Flshty-Hub/main/LICENSE
+--]]
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Flashtynooo/Flshty-Hub/refs/heads/main/lib"))()
