c44dbe1f87dfa25239dcd8cc24bb876cb9749d59
Flashtynooo <157314861+Flashtynooo@users.noreply.github.com>
2026-08-08 03:45:40 +0200

    Remove redundant loadstring call

--- .lua
+++ .lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Flashtynooo/Flshty-Hub/refs/heads/main/an1m-cryo-hub"))()
-wait(4)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Flashtynooo/Flshty-Hub/refs/heads/main/lib"))()
